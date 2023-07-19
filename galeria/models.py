d776231d763f34830f0363682e5ed193907a6a9d
Klaus Bentes <klausbentes@gmail.com>
2023-07-19 12:52:09 -0300

    alterações no banco de dados

--- galeria/models.py
+++ galeria/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Fotografia(models.Model):
-    nome: models.CharField(max_length=100, null=False, blank=False)
+    nome = models.CharField(max_length=100, null=False, blank=False, default='')
     legenda = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
