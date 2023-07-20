e733fdf0c1ca04d84d967bad260ab63ea84efa2e
Klaus Bentes <klausbentes@gmail.com>
2023-07-20 13:04:40 -0300

    Painel de admin personalizado ainda mais, e criada a opção de exibir ou não as publicações para o público.

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,9 +3,13 @@ from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
 
-    list_display = ('id', 'nome', 'legenda')
+    list_display = ('id', 'nome', 'legenda', 'publicada')
     list_display_links = ('id', 'nome')
     search_fields = ('nome', 'legenda')
+    list_filter = ('categoria',)
+    list_editable = ('publicada',)
+    list_per_page = 10
+
 
 # Register your models here.
 admin.site.register(Fotografia, ListandoFotografias)
